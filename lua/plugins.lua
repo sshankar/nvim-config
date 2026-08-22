6d300e7e48268de2a14b73b1d7688717c57f6a73
Shiv Shankar Menon Palat <shiv@pobox.com>
2026-08-22 13:37:42 -0400

    enable noice signature help with auto_open

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -15,6 +15,12 @@ return {
     },
     opts = {
       lsp = {
+        signature = {
+          enabled = true,
+          auto_open = {
+            enabled = true,
+          },
+        },
         override = {
           ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
           ['vim.lsp.util.stylize_markdown'] = true,
